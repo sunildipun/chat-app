ad589a0a90728f335c915add025cb949e8cba50c
Sunil <sunil.6318ie@gmail.com>
2018-09-21 13:23:30 +0530

    socket io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('Client disconneted');
+    })
+})
 
 var port = process.env.PORT || 3000;
 
 // console.log(__dirname + '/../public');
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`App is up at port ${port}`);
 })
